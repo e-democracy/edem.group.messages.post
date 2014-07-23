c96dd337d55ef87b4cf6ee26ceed1cc8d924bc4f
Bill Bushey <wbushey@gmail.com>
2014-07-23 18:34:14 -0500

    Making the content provider Python 3 ready

--- edem/group/messages/post/postcontentprovider.py
+++ edem/group/messages/post/postcontentprovider.py
@@ -1,6 +1,7 @@
 # coding=utf-8
+from __future__ import absolute_import, unicode_literals
 from gs.group.messages.post.postcontentprovider import GSPostContentProvider
-from canhide import can_hide_post
+from .canhide import can_hide_post
 
 
 class EDEMPostContentProvider(GSPostContentProvider):
